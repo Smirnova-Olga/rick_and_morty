1d1905b168162d60c1273d9ba2d9224351f1a946
Volha <volha@MacBook-Pro--Volha.local>
2023-03-31 14:38:08 +0200

    RIC-14 changes Text to locale in home_screen

--- lib/parts/home/home_part.dart
+++ lib/parts/home/home_part.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import 'package:rick_and_morty/gen/assets.gen.dart';
+import 'package:rick_and_morty/l10n/l10n.dart';
 import 'package:rick_and_morty/parts/characters/characters_part.dart';
 import 'package:rick_and_morty/parts/episodes/episodes_part.dart';
 import 'package:rick_and_morty/parts/locations/locations_part.dart';
